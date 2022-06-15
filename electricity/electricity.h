d4a552d504c3108be4d87514398bf113f3c941f0
Kageky <90093507+Kageky@users.noreply.github.com>
2022-06-15 17:28:37 +0300

    Update electricity.h

--- electricity/electricity.h
+++ electricity/electricity.h
@@ -2,51 +2,7 @@
 #include <string>
 
 class Object;
-
-/// <summary>
-/// Полюс устройства.
-/// К одному полюсу может быть подключен один любой другой полюс этого же
-/// устройства, или же любой полюс любого другого устройства.
-/// 
-/// У каждого полюса есть своё название, которое получает значение по умолчанию
-/// при создании устройства, но может быть изменено позднее.
-/// </summary>
-struct Pole {
-    /// <summary>
-    /// Название полюса.
-    /// Должно быть уникальным в пределах устройства.
-    /// </summary>
-    std::string name;
-
-    /// <summary>
-    /// Устройство, подключенное к данному полюсу.
-    /// Может совпадать с устройством, содержащим данный полюс, при условии, что соединяются
-    /// два разных полюса.
-    /// Значение <c>nullptr</c> означает, что к данному полюсу ничего не подключено.
-    /// </summary>
-    Object* connectedObject;
-
-    /// <summary>
-    /// Полюс устройства, к которому подключен данный полюс.
-    /// </summary>
-    std::string connectedObjectPole;
-
-    Pole(const std::string& name_) : name(name_), connectedObject(nullptr) {}
-};
-
-/// <summary>
-/// Электротехническое устройство.
-/// Имеет произвольное неотрицательное количество полюсов (<see cref="Pole"/>),
-/// через которые может соединяться с другими устройствами.
-/// </summary>
-class Object {
-    std::string name;
-
-protected:
-    Object(const std::string& name_) : name(name_) {}
-
-    /// <summary>
-    /// Возвращает полюс по внутреннему индексу устройства.
+	@@ -50,7 +50,7 @@ class Object {
     /// </summary>
     /// <param name="idx">Индекс полюса, от <c>0</c> до значения, возвращаемого <see cref="getPoleCount()"/>.</param>
     /// <returns>Полюс с указанным индексом, или <c>nullptr</c>, если такой полюс не существует.</returns>
@@ -54,12 +10,7 @@ protected:
 
     /// <summary>
     /// Возвращает полюс по внутреннему индексу устройства.
-    /// </summary>
-    /// <param name="idx">Индекс полюса, от <c>0</c> до значения, возвращаемого <see cref="getPoleCount()"/>.</param>
-    /// <returns>Полюс с указанным индексом, или <c>nullptr</c>, если такой полюс не существует.</returns>
-    virtual const Pole* getPole(size_t idx) const = 0;
-
-public:
+	@@ -63,8 +63,7 @@ class Object {
     virtual ~Object() {}
 
     const std::string& getName() const { return name; }
@@ -67,31 +18,7 @@ public:
     /// <summary>
     /// Возвращает полюс по имени.
     /// </summary>
-    /// <param name="name">Имя полюса.</param>
-    /// <returns>Полюс с указанным именем, или <c>nullptr</c>, если такой полюс не существует.</returns>
-    Pole* getPole(const std::string& name) { return const_cast<Pole*>(const_cast<const Object*>(this)->getPole(name)); }
-
-    /// <summary>
-    /// Возвращает полюс по имени.
-    /// </summary>
-    /// <param name="name">Имя полюса.</param>
-    /// <returns>Полюс с указанным именем, или <c>nullptr</c>, если такой полюс не существует.</returns>
-    virtual const Pole* getPole(const std::string& name) const = 0;
-
-    /// <returns>Количество полюсов в текущем устройстве.</returns>
-    virtual size_t getPoleCount() const = 0;
-
-    /// <summary>
-    /// Проверяет, связаны ли два указанных устройства напрямую через какую-либо пару их полюсов.
-    /// </summary>
-    /// <param name="other">Устройство, наличие прямой связи с которым проверяется.</param>
-    /// <returns><c>true</c> если устройства связаны напрямую, <c>false</c> в противном случае.</returns>
-    bool isConnectedTo(const Object& other) const;
-
-    /// <summary>
-    /// Соединяет указанные полюса текущего и указанного устройства.
-    /// Если к этим полюсам было что-то подключено, то такая связь разрушается.
-    /// </summary>
+	@@ -96,22 +95,20 @@ class Object {
     /// <param name="poleName">Название подключаемого полюса текущего устройства</param>
     /// <param name="other">Устройство, которое связывается с текущим.</param>
     /// <param name="otherPoleName">Название подключаемого полюса другого устройства</param>
@@ -112,22 +39,7 @@ public:
     bool disconnect(const std::string& poleName);
 };
 
-/// <summary>
-/// Простой выключатель с двумя полюсами.
-/// </summary>
-class Switch : public Object {
-public:
-    Pole a1, a2;
-
-    Switch(const std::string& name = "");
-
-    virtual size_t getPoleCount() const { return 2; }
-
-    virtual const Pole* getPole(const std::string& name) const;
-
-protected:
-    virtual const Pole* getPole(size_t idx) const;
-};
+	@@ -134,4 +131,32 @@ class Switch : public Object {
 
 // TODO: класс светильника с двумя полюсами
 
